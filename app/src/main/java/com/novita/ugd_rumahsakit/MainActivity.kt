efb24f5032b1d3a06117f5e17752d31cc6cc2a84
novitaaSarii <novitaasarii187@gmail.com>
2022-09-06 15:57:32 +0700

    update register

--- app/src/main/java/com/novita/ugd_rumahsakit/MainActivity.kt
+++ app/src/main/java/com/novita/ugd_rumahsakit/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.novita.ugd_rumahsakit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var username: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var email : TextInputEditText
+    private lateinit var tanggalLahir: TextInputEditText
+    private lateinit var nomorTelepon: TextInputEditText
+    private lateinit var btnRegister : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.etUsername)
+        password = findViewById(R.id.etPassword)
+        email = findViewById(R.id.etEmail)
+        tanggalLahir = findViewById(R.id.etTanggalLahir)
+        nomorTelepon = findViewById(R.id.etnomorTelepon)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, NextActivity::class.java)
+            val mBundle = Bundle()
+            mBundle.putString("username", username.text.toString())
+            mBundle.putString("password", password.text.toString())
+            mBundle.putString("email", email.text.toString())
+            mBundle.putString("tanggalLahir", tanggalLahir.text.toString())
+            mBundle.putString("nomorTelepon", nomorTelepon.text.toString())
+            intent.putExtra("register", mBundle)
+
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/novita/ugd_rumahsakit/NextActivity.kt
@@ -0,0 +1,11 @@
+package com.novita.ugd_rumahsakit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+    }
+}
