bd7856cec1a26abd51b1925f6940ae9fad7515e7
novitaaSarii <novitaasarii187@gmail.com>
2022-09-06 14:23:57 +0700

    Initial commit

---
+++ app/src/main/java/com/novita/ugd_rumahsakit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.novita.ugd_rumahsakit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
