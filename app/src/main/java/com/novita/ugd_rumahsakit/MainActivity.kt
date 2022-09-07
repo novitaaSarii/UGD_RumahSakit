340fcec07b74e7f7c371c388f3d6026f93229fbb
novitaaSarii <novitaasarii187@gmail.com>
2022-09-07 17:02:03 +0700

    Update KonsultasiKT

--- app/src/main/java/com/novita/ugd_rumahsakit/MainActivity.kt
+++ app/src/main/java/com/novita/ugd_rumahsakit/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var username: TextInputEditText
@@ -37,6 +39,32 @@ class MainActivity : AppCompatActivity() {
             intent.putExtras(Bundle)
 
             startActivity(intent)
+
+            //EXCEPTION HANDLING LOGIN
+            //private latenit var inputUsername: TextInputLayout
+            //private latenit var inputPassword: TextInputLayout
+            //private latenit var mainLayout: ConstraintLayout
+
+            // btnLogin.setOnClickListener(View.OnClickListener {
+                //var checkLogin = false
+                //val username: String = inputUsername.getEditText()?.getText().toString()
+                //val password: String = inputPassword.getEditText()?.getText().toString()
+
+                //if (username.isEmpty()) {
+                    //inputUsername.setError("Username must be filled with text")
+                    //checkLogin = false
+                //}
+
+                //if (password.isEmpty()) {
+                    //inputPassword.setError("Password must be filled with text")
+                    //checkLogin = false
+                //}
+
+                //if (username == "username" && password == "admin") checkLogin = true
+                //if (!checkLogin) return@OnClickListener
+                //val moveHome = Intent(this@MainActivity, HomeActivity::class.java)
+                //startActivity(moveHome)
+            //})
         }
     }
 }
