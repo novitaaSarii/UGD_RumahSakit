b122cfa0c208712c8a8b21ea9714cf3edb53630a
Henri <henritom1301@gmail.com>
2022-09-23 19:53:36 +0700

    CRUD no 6 belum selesai

---
+++ app/src/main/java/com/novita/ugd_rumahsakit/CRUD/DokterAdapter.kt
@@ -0,0 +1,16 @@
+/*
+belum jadi
+package com.novita.ugd_rumahsakit.CRUD
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class DokterAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dokter_adapter)
+    }
+}
+
+ */
